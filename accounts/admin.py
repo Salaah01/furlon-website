fb2ac08e1e6b7bd8f5dc381ae6b92a41bcf872dd
Salaah <salaah_amin@hotmail.co.uk>
2019-10-14 23:40:04 +0100

    pylint

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,16 +1,16 @@
 """
-=======================================================================================================================
+==================================================================================================================================
 Date Created:   26/09/2019
 Created By:     Salaah Amin
 Last Update:    26/09/2019
-=======================================================================================================================
+==================================================================================================================================
 SCRIPT FUNCTION
 
 Will allow access to the following tables via the admin page:
     - CustomerDetails: accounts_customerdetails
     - StoreUsers: accounts_storeusers
     - StoreRights: accounts_storerights
-=======================================================================================================================
+==================================================================================================================================
 """
 
 # IMPORTS
@@ -23,68 +23,28 @@ from django.contrib import admin
 from .models import CustomerDetails, StoreRights, StoreUsers
 
 
-###############################################################################
+##################################################################################################################################
 class CustomerDetailsAdmin(admin.ModelAdmin):
     """ Registers Customer Details (accounts_customerdetails) table to the
     admin page """
-    list_display = (
-        'user_id',
-        'city',
-        'postcode',
-        'country',
-        'status',
-        'verified',
-        'opt_in'
-    )
-    list_display_links = (
-        'user_id',
-    )
-    list_filter = (
-        'verified',
-        'status',
-        'opt_in',
-    )
-    list_editable = (
-        'verified',
-        'opt_in',
-    )
-    search_fields = (
-        'user_id__id',
-        'user_id__first_name',
-        'user_id__last_name',
-        'user_id__email',
-        'user_id__username',
-    )
+    list_display = ('user_id', 'city', 'postcode', 'country', 'status', 'verified', 'opt_in')
+    list_display_links = ('user_id', )
+    list_filter = ('verified', 'status', 'opt_in', )
+    list_editable = ('verified', 'opt_in', )
+    search_fields = ('user_id__id', 'user_id__first_name', 'user_id__last_name', 'user_id__email', 'user_id__username', )
 
 
-###############################################################################
+##################################################################################################################################
 class StoreUserAdmin(admin.ModelAdmin):
     """ Registers Store User (accounts_storeuser) table to the admin page """
-    list_display = (
-        'user_id',
-        'store_id',
-        'title',
-        'rights',
-        'status'
-    )
-    list_display_links = (
-        'user_id',
-    )
-    list_filter = (
-        'status',
-    )
-    search_fields = (
-        'user_id__first_name',
-        'user_id__last_name',
-        'user_id__email',
-        'user_id__username',
-        'store_id__name',
-        'store_id__id',
-        'store_id__postcode',
-    )
+    list_display = ('user_id', 'store_id', 'title', 'rights', 'status')
+    list_display_links = ('user_id', )
+    list_filter = ('status', )
+    search_fields = ('user_id__first_name', 'user_id__last_name', 'user_id__email', 'user_id__username', 'store_id__name',
+                     'store_id__id', 'store_id__postcode', )
 
 
-###############################################################################
+##################################################################################################################################
 class StoreRightsAdmin(admin.ModelAdmin):
     """
     Registers Store Rights (accounts_storerights) table to the admin page
@@ -92,6 +52,7 @@ class StoreRightsAdmin(admin.ModelAdmin):
     list_display = ('rights',)
     list_display_links = ('rights',)
     search_fields = ('rights',)
+
 
 admin.site.register(CustomerDetails, CustomerDetailsAdmin)
 admin.site.register(StoreUsers, StoreUserAdmin)
