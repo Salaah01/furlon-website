89693c98f5aae1ae7942d6764913c2bd1537c0b7
Salaah <salaah_amin@hotmail.co.uk>
2019-12-04 19:38:12 +0000

    removed keybinding events on the window - testing a fix

--- furlon/static/js/components/navigation.js
+++ furlon/static/js/components/navigation.js
@@ -26,41 +26,6 @@ var __values = (this && this.__values) || function(o) {
 var e_1, _a;
 // Close sub-menus when user has clicked out of the navigation.
 var navSubMenus = document.querySelectorAll(".nav__option__dropdown-opts");
-window.addEventListener("mouseup", function (event) {
-    var e_2, _a;
-    var target = event.target;
-    event.stopPropagation();
-    event.stopImmediatePropagation();
-    try {
-        //   console.log(target.classList.contains('nav__option__btn'))
-        for (var _b = __values(navSubMenus), _c = _b.next(); !_c.done; _c = _b.next()) {
-            var navSubMenu = _c.value;
-            if (!target.classList.contains('nav__option__btn') && (target.parentNode) != navSubMenu) {
-                navSubMenu.classList.add("nav__option__dropdown-opts--collapse");
-                navSubMenu.classList.remove("nav__option__dropdown-opts--expanded");
-            }
-            console.log(target.classList.contains('nav__option__btn') && navSubMenu.classList.contains('nav__option__dropdown-opts--expanded'));
-            console.log(target.classList.contains('nav__option__btn') && navSubMenu.classList.contains('nav__option__dropdown-opts--collapse'));
-            if (target.classList.contains('nav__option__btn') && navSubMenu.classList.contains('nav__option__dropdown-opts--expanded')) {
-                console.log(1);
-                navSubMenu.classList.add("nav__option__dropdown-opts--collapse");
-                navSubMenu.classList.remove("nav__option__dropdown-opts--expanded");
-            }
-            else if (target.classList.contains('nav__option__btn') && navSubMenu.classList.contains('nav__option__dropdown-opts--collapse')) {
-                console.log(2);
-                navSubMenu.classList.remove("nav__option__dropdown-opts--collapse");
-                navSubMenu.classList.add("nav__option__dropdown-opts--expanded");
-            }
-        }
-    }
-    catch (e_2_1) { e_2 = { error: e_2_1 }; }
-    finally {
-        try {
-            if (_c && !_c.done && (_a = _b.return)) _a.call(_b);
-        }
-        finally { if (e_2) throw e_2.error; }
-    }
-});
 function subMenuCtrl(event) {
     // Function will handle click events on the navigation for expanding
     // and collapasing the sub-menus.
--- furlon/static/ts/components/navigation.ts
+++ furlon/static/ts/components/navigation.ts
@@ -15,32 +15,6 @@
 // Close sub-menus when user has clicked out of the navigation.
 const navSubMenus = document.querySelectorAll(".nav__option__dropdown-opts");
 
-window.addEventListener("mouseup", event => {
-  let target = event.target as HTMLElement;
-  event.stopPropagation();
-  event.stopImmediatePropagation();
-//   console.log(target.classList.contains('nav__option__btn'))
-  for (const navSubMenu of navSubMenus as any) {
-    
-    if (!target.classList.contains('nav__option__btn') && (target.parentNode) != navSubMenu) {
-      navSubMenu.classList.add("nav__option__dropdown-opts--collapse");
-      navSubMenu.classList.remove("nav__option__dropdown-opts--expanded");
-
-    } 
-    console.log(target.classList.contains('nav__option__btn') && navSubMenu.classList.contains('nav__option__dropdown-opts--expanded'))
-    console.log(target.classList.contains('nav__option__btn') && navSubMenu.classList.contains('nav__option__dropdown-opts--collapse'))
-    if (target.classList.contains('nav__option__btn') && navSubMenu.classList.contains('nav__option__dropdown-opts--expanded')) {
-        console.log(1)
-        navSubMenu.classList.add("nav__option__dropdown-opts--collapse");
-        navSubMenu.classList.remove("nav__option__dropdown-opts--expanded");
-    } else if (target.classList.contains('nav__option__btn') && navSubMenu.classList.contains('nav__option__dropdown-opts--collapse')) {
-        console.log(2)
-        navSubMenu.classList.remove("nav__option__dropdown-opts--collapse");
-        navSubMenu.classList.add("nav__option__dropdown-opts--expanded");
-    }
-  }
-});
-
 function subMenuCtrl(event: MouseEvent) {
   // Function will handle click events on the navigation for expanding
   // and collapasing the sub-menus.
