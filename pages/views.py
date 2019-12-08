d29ed2592b2481d8a641b89959c215fdd9d1f28c
Salaah <salaah_amin@hotmail.co.uk>
2019-12-08 00:55:59 +0000

    initial build of index

--- pages/views.py
+++ pages/views.py
@@ -13,10 +13,14 @@ index:      /
 """
 
 from django.shortcuts import render
+from products.models import Products
 
 
 # ------------------------------------------------------------------------------------------------------------------------------ #
 def index(request):
     """ Loads the Index Page """
-    context = {}
+    latest_products = Products.objects.order_by('-upload_date').filter(status='Active')[:6]
+    context = {
+        'latest_products': latest_products
+    }
     return render(request, 'pages/index.html', context)
