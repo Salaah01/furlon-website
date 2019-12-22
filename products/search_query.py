ab1a1682a08ea419f363c5c405d34bfe433bd99e
Salaah <salaah_amin@hotmail.co.uk>
2019-12-22 06:13:32 +0000

    put the sql search in its own module and made the whole thing more dry

---
+++ products/search_query.py
@@ -0,0 +1,123 @@
+"""
+================================================================================================================================
+Date Created:   22/12/2019
+Created By:     Salaah Amin
+================================================================================================================================
+SCRIPT FUNCTION
+
+Queries the database and returns search results based on user inputs.
+================================================================================================================================
+"""
+
+# Python Library Imports
+from datetime import datetime
+import json
+
+# Third Party Library Imports
+from django.db import connection
+
+# Local Imports
+from .models import Products
+
+
+# ------------------------------------------------------------------------------------------------------------------------------ #
+def search_query(criteria, jsonResponse=False):
+    """ Given some criteria (from request.GET), the database will be queried, the results of which will be returned. """
+    # Filter Options
+    minPrice = criteria['f-minPrice'] if 'f-minPrice' in criteria else 0
+    maxPrice = criteria['f-maxPrice'] if 'f-maxPrice' in criteria else 9999999999
+    category = criteria['f-category'] if 'f-category' in criteria else None
+    colour = criteria['f-colour'] if 'f-colour' in criteria else None
+    
+    # Search Option
+    # - It is assumed that the delimiter between keywords will be either ', ' or ','.
+    # - Each keyword should check the value somewhat matches a value from various fields.
+    # - Where the keyword = 'x', the search should be similar to <field> IS LIKE '%x%'.
+    if 'search' in criteria:
+        search = criteria['search']
+        search = search.replace(' ', '').split(',') if ',' in search else search.split()
+        searchSQL = 'AND ('
+        searchBindVars = []
+        for keyword in search:
+            searchBindVars += searchBindVars + ['%' + keyword + '%'] * 5
+            searchSQL += """
+                LOWER(pp.name) like %s
+                OR  (
+                    SELECT COUNT (*)
+                    FROM stores_stores ss
+                    WHERE pp.store_id = ss.store_id
+                    AND LOWER(ss.name) like %s
+                ) > 0
+                OR  (
+                    SELECT COUNT (*)
+                    FROM products_colours pcol
+                    WHERE pp.colour_id = pcol.id
+                    AND (lower(pcol.name) like %s OR lower(pcol.col_families) like %s)
+                ) > 0
+                OR (
+                    SELECT COUNT (*)
+                    FROM products_categories pcat
+                    WHERE pp.category_id = pcat.cat_id
+                    AND pcat.name like %s
+                ) > 0
+            )
+            """
+    else:
+        searchBindVars = []
+        searchSQL = ''
+
+    # fullSQL consists of an sql build based on the filters and the searchSQL.
+    # As a list of bindVars is used as supposed to a dictionary of bindVars, order matters.
+    # This is done to prevent SQL injection when building the search query.
+    # Note: The fields selected are the same as the filters set in the search page.
+    
+    # NOTE: MAIN COLOUR IS SET TO TRUE OR FALSE IN DEV, REMOVE WHEN THERE ARE ENOUGH PRODUCTS
+    fullSQL = """
+        SELECT
+            pp.product_id, pp.name pp_name, pp.height, pp.length, pp.width, pp.features, pp.related,
+            pp.showcase_image, pp.description, pp.price, pp.rating, TO_CHAR(pp.upload_date, 'dd/mm/yyyy'), pp.inventory, pp.status,
+            pp.category_id, pp.colour_id, pp.store_id, pp.delivery_available, pp.main_colour, pp.ratings,
+            ss.name ss_name
+        FROM products_products pp, stores_stores ss
+        WHERE pp.price >= %s
+        AND pp.price <= %s
+        AND (pp.main_colour = true OR pp.main_colour = false)
+        AND pp.status = 'Active'
+        AND pp.store_id = ss.store_id
+        """
+
+    if category:
+        fullSQL += "AND pp.category_id %s"
+    if colour:
+        fullSQL += "AND pp.colour_id %s"
+
+    fullSQL += searchSQL
+
+    bindVars = [float(minPrice), float(maxPrice)]
+    if category:
+        bindVars.append(category)
+    if colour:
+        bindVars.append(colour)
+    bindVars = bindVars + searchBindVars
+
+    cursor = connection.cursor()
+    cursor.execute(fullSQL, bindVars)
+    dataRows = cursor.fetchall()
+
+    if jsonResponse:
+        cursor = connection.cursor()
+        cursor.execute(fullSQL, bindVars)
+        dataRows = cursor.fetchall()
+        keys = ('product_id',)
+
+        results = []
+        for dataRow in dataRows:
+            results.append(dict(zip(keys, dataRow)))
+        
+        
+        jsonData = json.dumps(results)
+
+        return jsonData
+    else:
+
+        return Products.objects.raw(fullSQL, bindVars)
--- products/views.py
+++ products/views.py
@@ -16,10 +16,11 @@ This includes the search page and a dedicated page for each product.
 # Third Party Imports
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.http import JsonResponse
+from django.http import HttpResponse
 
 # Local Imports
 from products.models import Products, Categories, ColourFamilies
+from .search_query import search_query
 
 
 # ------------------------------------------------------------------------------------------------------------------------------ #
@@ -35,7 +36,7 @@ def search(request):
                 pp.category_id, pp.colour_id, pp.store_id, pp.delivery_available, pp.main_colour, pp.ratings,
                 ss.name ss_name
         FROM products_products pp, stores_stores ss
-        WHERE pp.main_colour = true OR pp.main_colour = false)
+        WHERE (pp.main_colour = true OR pp.main_colour = false)
         AND pp.status = 'Active'
         AND pp.store_id = ss.store_id
         """
@@ -43,46 +44,13 @@ def search(request):
 
     # Retrieving results based on the search query.
     if 'search' in request.GET:
-        if request.GET['search']:
-            searchTerms = request.GET['search']
-            searchTerms = searchTerms.replace(' ', '').split(',') if ',' in searchTerms else searchTerms.split()
-            for searchTerm in searchTerms:
-                # NOTE: MAIN COLOUR IS SET TO TRUE OR FALSE IN DEV, REMOVE WHEN THERE ARE ENOUGH PRODUCTS
-                results = Products.objects.raw(
-                    """
-                    SELECT
-                        pp.product_id, pp.name pp_name, pp.height, pp.length, pp.width, pp.features, pp.related,
-                        pp.showcase_image, pp.description, pp.price, pp.rating, pp.upload_date, pp.inventory, pp.status,
-                        pp.category_id, pp.colour_id, pp.store_id, pp.delivery_available, pp.main_colour, pp.ratings,
-                        ss.name ss_name
-                    FROM products_products pp, stores_stores ss
-                    WHERE ( pp.main_colour = true OR pp.main_colour = false)
-                    AND pp.status = 'Active'
-                    AND pp.store_id = ss.store_id
-                    AND (
-                            LOWER(pp.name) like %(search)s
-                        OR  (
-                            SELECT COUNT (*)
-                            FROM stores_stores ss
-                            WHERE pp.store_id = ss.store_id
-                            AND LOWER(ss.name) like %(search)s
-                        ) > 0
-                        OR  (
-                            SELECT COUNT (*)
-                            FROM products_colours pcol
-                            WHERE pp.colour_id = pcol.id
-                            AND (lower(pcol.name) like %(search)s OR lower(pcol.col_families) like %(search)s)
-                        ) > 0
-                        OR (
-                            SELECT COUNT (*)
-                            FROM products_categories pcat
-                            WHERE pp.category_id = pcat.cat_id
-                            AND pcat.name like %(search)s
-                        ) > 0
-                    )
-                    """,
-                    {'search': '%' + searchTerm.lower() + '%'}
-                )
+        query = {}
+        if 'search' in request.GET:
+            query[search] = request.GET['search']
+
+        criterion = ['search', 'f-minPrice', 'f-maxPrice', 'f-category', 'f-colour']
+        query = {criteria: request.GET[criteria] for criteria in criterion if request.GET[criteria]}
+        results = search_query(query)
 
     # Pagination
     paginator = Paginator(results, 20)
@@ -114,87 +82,8 @@ def search_results(request):
     """ Returns user's search results as a JSON object.
     The results will be generated based on what has been submitted using the search bar and the filters.
     """
-    # Filter Options
-    minPrice = request.GET['minPrice'] if 'minPrice' in request.GET else 0
-    maxPrice = request.GET['maxPrice'] if 'maxPrice' in request.GET else 9999999999
-    category = request.GET['category'] if 'category' in request.GET else None
-    colour = request.GET['colour'] if 'colour' in request.GET else None
+    criterion = ['search', 'f-minPrice', 'f-maxPrice', 'f-category', 'f-colour']
+    query = {criteria: request.GET[criteria] for criteria in criterion if request.GET[criteria]}
 
-
-    # Search Option
-    # - It is assumed that the delimiter between keywords will be either ', ' or ','.
-    # - Each keyword should check the value somewhat matches a value from various fields.
-    # - Where the keyword = 'x', the search should be similar to <field> IS LIKE '%x%'.
-    if 'search' in request.GET:
-        search = request.GET['search']
-        search = search.replace(' ', '').split(',') if ',' in search else search.split()
-        searchSQL = 'AND ('
-        searchBindVars = []
-        for keyword in search:
-            searchBindVars += searchBindVars + ['%' + keyword + '%'] * 5
-            searchSQL += """
-                LOWER(name) like %s
-                OR  (
-                    SELECT COUNT (*)
-                    FROM stores_stores ss
-                    WHERE pp.store_id = ss.store_id
-                    AND LOWER(ss.name) like %s
-                ) > 0
-                OR  (
-                    SELECT COUNT (*)
-                    FROM products_colours pcol
-                    WHERE pp.colour_id = pcol.id
-                    AND (lower(pcol.name) like %s OR lower(pcol.col_families) like %s)
-                ) > 0
-                OR (
-                    SELECT COUNT (*)
-                    FROM products_categories pcat
-                    WHERE pp.category_id = pcat.cat_id
-                    AND pcat.name like %s
-                ) > 0
-            )
-            """
-    else:
-        searchBindVars = []
-        searchSQL = ''
-
-    # fullSQL consists of an sql build based on the filters and the searchSQL.
-    # As a list of bindVars is used as supposed to a dictionary of bindVars, order matters.
-    # This is done to prevent SQL injection when building the search query.
-    # Note: The fields selected are the same as the filters set in the search page.
-    fullSQL = """
-        SELECT pp.product_id, pp.price, pp.category_id
-        FROM products_products pp
-        WHERE pp.price >= %s
-        AND pp.price <= %s
-        """
-    if category:
-        fullSQL += "AND category_id %s"
-    if colour:
-        fullSQL += "AND colour_id %s"
-
-    fullSQL += searchSQL
-
-    bindVars = [float(minPrice), float(maxPrice)]
-    if category:
-        bindVars.append(category)
-    if colour:
-        bindVars.append(colour)
-    bindVars = bindVars + searchBindVars
-
-    from django.db import connection
-    import json
-    from django.http import HttpResponse
-    cursor = connection.cursor()
-    print(fullSQL)
-    print(bindVars)
-    cursor.execute(fullSQL, bindVars)
-    dataRows = cursor.fetchall()
-    # Keys = taken from products_products. Only include the headers in the filter to reduce download speed.
-    keys = ('product_id', 'price', 'category_id')
-    result = []
-    for dataRow in dataRows:
-        result.append(dict(zip(keys, dataRow)))
-
-    json_data = json.dumps(result)
-    return HttpResponse(json_data, content_type="application/json")
+    searchResults = search_query(query, True)
+    return HttpResponse(searchResults, content_type="application/json")
