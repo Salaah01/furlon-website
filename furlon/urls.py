14c42d82a0c7cbee69bfb15be792741c57570bef
Amin <salaah.amin@towerswatson.com>
2020-01-08 14:57:40 +0000

    updated the store_ratings partial to use store.rating rather than products.ratings. Moved the filed to the partials folder. Started work on the store page including the mvc and some content

--- furlon/urls.py
+++ furlon/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('products/', include('products.urls')),
+    path('stores/', include('stores.urls')),
     path('user/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
